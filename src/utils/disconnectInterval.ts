ae113c76d57795c572d8b3dc0ccc642f5ac84d69
Insamedkv <dab.sky1997@gmail.com>
2021-10-06 19:31:52 +0300

    refactor: disconnect socket method import

--- src/utils/disconnectInterval.ts
+++ src/utils/disconnectInterval.ts
@@ -1,7 +1,6 @@
 import { Socket } from 'socket.io';
 import { RoomModel } from '../models/room';
 import { UserModel } from '../models/user';
-import cloudinary from './cloudinary';
 import { disconnect } from './usersSocket';
 import { Event } from '../constants';
 
@@ -10,7 +9,7 @@ export async function discon(socketId: string, socket: Socket) {
     const userId = disconnect(socketId);
     if (userId === undefined) return;
     const room = await RoomModel.getRoomByUser(userId);
-    const user = await UserModel.deleteUserById(userId);
+    await UserModel.deleteUserById(userId);
     await RoomModel.deleteUserFromRoomById(userId);
     const users = await RoomModel.getRoomUsers(room.id);
     await socket.to(room.id).emit(Event.USER_DELETE, users);
